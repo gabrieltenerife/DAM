9adf60602e2fcf11ffb7df12e86bf5614df2a5ca
GabrielGlez <gabrieltenerife123@gmail.com>
2024-09-25 23:59:22 +0100

    Avance

--- Segundo Año/Dispositivos multimedia/CalculadoraMovil/app/src/main/java/com/example/calculadoraintento2/MainActivity.kt
+++ Segundo Año/Dispositivos multimedia/CalculadoraMovil/app/src/main/java/com/example/calculadoraintento2/MainActivity.kt
@@ -1,33 +1,98 @@
 package com.example.calculadoraintento2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     // Creamos las variables
-    /*
-    lateinit var et1: EditText
-    lateinit var et2: EditText
-    lateinit var tv1: TextView
-    lateinit var button: Button
-    */
+    lateinit var Numa: EditText
+    lateinit var Numb: EditText
 
+    lateinit var Sum: Button
+    lateinit var Res: Button
+    lateinit var Mul: Button
+    lateinit var Divi: Button
+    lateinit var Limp: Button
+
+    lateinit var Pantalla: TextView
+    lateinit var Alerta: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Enlazamos con el xml
-        /*
-        et1 = findViewById(R.id.Numero)
-        et2 = findViewById(R.id.Numero1)
-        tv1 = findViewById(R.id.Resultado)
-        button = findViewById(R.id.Boton)
-        */
+        Numa = findViewById(R.id.numa)
+        Numb = findViewById(R.id.numb)
+
+        Sum = findViewById(R.id.sum)
+        Res = findViewById(R.id.res)
+        Mul = findViewById(R.id.mul)
+        Divi = findViewById(R.id.div)
+        Limp = findViewById(R.id.clear)
+
+        Pantalla = findViewById(R.id.pantalla)
+        Alerta = findViewById(R.id.alert)
+
+        //Listeners
+        Sum.setOnClickListener {
+            suma()
+        }
+
+        Res.setOnClickListener{
+            resta()
+        }
+
+        Limp.setOnClickListener{
+            clear()
+        }
+
+
+
     }
 
+    //Metodos
+    @SuppressLint("SetTextI18n")
+    fun suma (){
+        var valor1 = Numa.text.toString()
+        var valor2 = Numb.text.toString()
+        var resultado = 0
+
+        println(valor1)
+
+        if (valor1.isNotEmpty() && valor2.isNotEmpty()){
+            resultado = valor1.toInt() + valor2.toInt()
+            Pantalla.text = "Resultado es ${resultado.toString()}"
+        } else Alerta.text = "Introduce ambos numeros para operar"
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun resta () {
+        var valor1 = Numa.text.toString()
+        var valor2 = Numb.text.toString()
+        var resultado = 0
+
+        if(valor1.isNotEmpty() && valor2.isNotEmpty()){
+
+            if (valor1.toInt() > valor2.toInt()) {
+                resultado = valor1.toInt() - valor2.toInt()
+                Pantalla.text = "El resultado es ${resultado.toString()}"
+            } else {
+                resultado = valor2.toInt() - valor1.toInt()
+                Pantalla.text = "El resultado es ${resultado.toString()}"
+            }
+        } else Alerta.text = "Introduce ambos numeros para operar"
+    }
+
+    fun clear () {
+        Pantalla.text = ""
+    }
 
 }
